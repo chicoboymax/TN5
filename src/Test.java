1e33f4d55425a01a1150f4910d76293f6b8aa785
Maxime Drouin <chicoboymax@gmail.com>
2016-03-10 07:25:02 -0500

    Comments

--- src/Test.java
+++ src/Test.java
@@ -18,33 +18,29 @@ import java.util.Scanner;
  */
 
 public class Test {
+	// Scanner utilisé pour la saisie des données au clavier
 	static Scanner sc = new Scanner(System.in);
 
-	// Grille utilisée si le joueur choisi l'option jouerSudoku avant d'avoir
-	// chargé le jeu
-	public static int[][] initialiserGrille(){
-		int[][] grille = new int[9][9];
-		for (int i=0;i<grille.length;i++){
-			Arrays.fill(grille[i], 0);
-		}
-		return grille;
-	}
-	
-
 	public static void main(String[] args) {
-		menuPrincipal();
+		// Appel de la méthode contenant le menu principal
+		menuPrincipal(1, 2, 3);
 	}
 
 	/*********************************************************************************/
 	/*
+	 * Affiche le menu principal ainsi que ces choix.
 	 * 
+	 * @param inf - Le chiffre du choix le plus bas
 	 * 
+	 * @param mid - Le chiffre du choix du milieu
+	 * 
+	 * @param sup - Le chiffre du choix le plus haut
 	 */
 	/*********************************************************************************/
-	public static void menuPrincipal() {
+	public static void menuPrincipal(int inf, int mid, int sup) {
 		final String VOTRE_CHOIX = "Votre choix: ";
 		int nombre;
- 
+
 		System.out
 				.println("*************sudoku***************************......");
 		System.out
@@ -54,21 +50,26 @@ public class Test {
 		System.out.println("1. Charger jeu.");
 		System.out.println("2. Jouer Sudoku.");
 		System.out.println("3. Quitter.");
+		// Utilise la méthode ObtenirUnNombre pour obtenir et valider la saisie
+		// des nombres
+		nombre = ObtenirUnNombre(inf, sup);
 
-		nombre = ObtenirUnNombre(1, 3);
-
-		if (nombre == 1) {
-			System.out.println(VOTRE_CHOIX + 1);
-
-			menuSecondaire(1, 2, 3, chargerJeu());
-
-		} else if (nombre == 2) {
-			System.out.println(VOTRE_CHOIX + 2);
-			Grille sudoku = new Grille(Grille.grilleToArrayList(initialiserGrille()));
-
-			jouerSudoku(sudoku);
+		// Si choix 1 appelle la méthode menuSecondaire et lui passe en
+		// paramètre la méthode chargerJeu
+		if (nombre == inf) {
+			System.out.println(VOTRE_CHOIX + inf);
+			menuSecondaire(inf, mid, sup, chargerJeu());
+			// Si choix 2 appelle la méthode menuSecondaire et créer une
+			// nouvelle instance de Grille à partie de la méthode
+			// initialiserGrille.
+		} else if (nombre == mid) {
+			System.out.println(VOTRE_CHOIX + mid);
+			Grille sudoku = new Grille(
+					Grille.grilleToArrayList(initialiserGrille()));
+			menuSecondaire(inf, mid, sup, sudoku);
+			// Si choix 3 remercie le joueur d'avoir joué et sort du programme
 		} else {
-			System.out.println(VOTRE_CHOIX + 3);
+			System.out.println(VOTRE_CHOIX + sup);
 			System.out.println("Merci d’avoir joué au jeu sudoku.");
 			System.exit(0);
 		}
@@ -76,16 +77,26 @@ public class Test {
 
 	/*********************************************************************************/
 	/*
+	 * Affiche le menu principal ainsi que ces choix.
 	 * 
+	 * @param inf - Le chiffre du choix le plus bas
 	 * 
+	 * @param mid - Le chiffre du choix du milieu
+	 * 
+	 * @param sup - Le chiffre du choix le plus haut
+	 * 
+	 * @param sudoku - La grille crée par le menu principal
+	 * 
+	 * @return boolean - Affiche le menu après chaque itération.
 	 */
 	/*********************************************************************************/
 	public static boolean menuSecondaire(int inf, int mid, int sup,
 			Grille sudoku) {
-
+		// Affiche le menu après chaque itération.
 		while (true) {
 			System.out
 					.println("\n-------------------------------------------------------------------------------");
+			// Imprime la grille
 			sudoku.imprimerGrille();
 			final String VOTRE_CHOIX = "Votre choix: ";
 			int nombre;
@@ -98,11 +109,13 @@ public class Test {
 
 			if (nombre == inf) {
 				System.out.println(VOTRE_CHOIX + inf);
-				System.out.println("Veuillez fournir le nom du fichier:");
-				String nomFichier = sc.next();
-				sauvegarderJeu(nomFichier, sudoku.getGrid());
+				// Appelle la méthode sauvegarderJeu et lui passe en paramètre
+				// la grille du jeu
+				sauvegarderJeu(sudoku.getGrid());
 			} else if (nombre == mid) {
 				System.out.println(VOTRE_CHOIX + mid);
+				// Appelle la méthode jouerSudoku et lui passe le sudoku en
+				// paramètre
 				jouerSudoku(sudoku);
 			} else {
 				System.out.println(VOTRE_CHOIX + sup);
@@ -115,35 +128,65 @@ public class Test {
 
 	/*********************************************************************************/
 	/*
+	 * Permet de charger une grille sauvegardée dans un fichier texte.
 	 * 
-	 * 
+	 * @return - Grille de sudoku générè à partie d'un fichier sauvegardé
 	 */
 	/*********************************************************************************/
 	public static Grille chargerJeu() {
+		// Utilise la méthode fairelalecture pour obtenir un BufferedReader
 		BufferedReader br = fairelalecture();
+		// Créer un ArrayList de cases à partir du BufferedReader en utilisant
+		// la méthode lireFichier
 		ArrayList<Case> al = lireFichier(br);
+		// Créer une nouvelle grille en utilisant l'ArrayList de cases.
 		Grille sudoku = new Grille(al);
+		// Indique au joueur que la partie a été chargée
 		System.out.println("Fichier chargé! \n");
 		return sudoku;
 	}
 
 	/*********************************************************************************/
 	/*
+	 * Initialise une grille de sudoku dans le cas où le joueur choisi l'option
+	 * jouer sudoku avant d'avoir chargé une grille.
+	 */
+	/*********************************************************************************/
+	public static int[][] initialiserGrille() {
+		int[][] grille = { { 0, 6, 0, 1, 0, 4, 0, 5, 0 },
+				{ 0, 0, 8, 3, 0, 5, 6, 0, 0 }, { 2, 0, 0, 0, 0, 0, 0, 0, 1 },
+				{ 8, 0, 0, 4, 0, 7, 0, 0, 6 }, { 0, 0, 6, 0, 0, 0, 3, 0, 0 },
+				{ 7, 0, 0, 9, 0, 1, 0, 0, 4 }, { 5, 0, 0, 0, 0, 0, 0, 0, 2 },
+				{ 0, 0, 7, 2, 0, 6, 9, 0, 0 }, { 0, 4, 0, 5, 0, 8, 0, 7, 0 } };
+		return grille;
+	}
+
+	/*********************************************************************************/
+	/*
+	 * Créer un BufferedReader à partir du nom d'un fichier demandé à
+	 * l'utilisateur
 	 * 
-	 * 
+	 * @return entree - le bufferedReader
 	 */
 	/*********************************************************************************/
 	private static BufferedReader fairelalecture() {
 		BufferedReader entree = null;
 		boolean flag = true;
+		// Exécute la boucle tant que le flag est vrai
 		while (flag) {
 			try {
 				System.out.println("Veuillez fournir le nom du fichier:");
 				String nomFichier = sc.next();
+				// Créer un fichier
 				File unfichier = new File(nomFichier);
+				// Créer un bufferedReader à partir d'un fileReader utilisant le
+				// fichier
 				entree = new BufferedReader(new FileReader(unfichier));
+				// Si la lecture est réussie met le flag à false et sort de la
+				// boucle.
 				flag = false;
 			} catch (FileNotFoundException e) {
+				// Si ne trouve pas le fichier affiche un message l'indiquant
 				System.out.println("Erreur, fichier inexistant! ");
 			}
 
@@ -153,8 +196,11 @@ public class Test {
 
 	/*********************************************************************************/
 	/*
+	 * Créer un ArrayList de cases à Partir du BufferedReader
 	 * 
+	 * @param fluxEntree - Le BufferedReader
 	 * 
+	 * @return alCase - Un ArrayList de cases
 	 */
 	/*********************************************************************************/
 	private static ArrayList<Case> lireFichier(BufferedReader fluxEntree) {
@@ -163,21 +209,33 @@ public class Test {
 		String line = " ";
 		String[] donnee;
 		try {
+			// Li la ligne du fichier
 			line = fluxEntree.readLine();
 		} catch (IOException e) {
 			System.out.println("Erreur de lecture");
 			System.exit(0);
 		}
+		// Renvoi null si la ligne est vide
 		if (line == null) {
 			return null;
 		} else {
+			// Split le contenu de la ligne à chaque "Espace" et met le résultat
+			// dans l'Array donnee
 			donnee = line.split(" ");
 
+			// Pour chaque entrée de "donnee"
 			for (String placement : donnee) {
+				// Assigne le premier chiffre de la donnee à la variable ligne
 				ligne = placement.charAt(0) - '0';
+				// Assigne le deuxième chiffre de la donnee à la variable
+				// colonne
 				colonne = placement.charAt(1) - '0';
+				// Assigne le troisième chiffre de la donnee à la variable
+				// valeur
 				valeur = placement.charAt(2) - '0';
+				// Créer une case à partir des 3 données
 				Case caseActuelle = new Case(ligne, colonne, valeur);
+				// Ajoute cette case à l'ArrayList
 				alCase.add(caseActuelle);
 			}
 		}
@@ -191,7 +249,9 @@ public class Test {
 	 * 
 	 */
 	/*********************************************************************************/
-	public static void sauvegarderJeu(String cheminDuFichier, int[][] grid) {
+	public static void sauvegarderJeu(int[][] grid) {
+		System.out.println("Veuillez fournir le nom du fichier:");
+		String cheminDuFichier = sc.next();
 		ArrayList<Case> al = Grille.grilleToArrayList(grid);
 		File fichier = new File(cheminDuFichier);
 		PrintWriter sortie = null;
@@ -240,7 +300,7 @@ public class Test {
 	/*********************************************************************************/
 	public static int obtenirNombrePlusPetitOuEgalA(int sup, String rowColVal) {
 		final String message = "Entrez un chiffre pour la " + rowColVal
-				+ ", il doit être compris entre 0 et " + sup + ".";
+				+ ", il doit être compris entre 1 et " + sup + ".";
 		final String message1 = "Veuillez choisir une " + rowColVal + ":";
 		int nombre = 0;
 		do {
